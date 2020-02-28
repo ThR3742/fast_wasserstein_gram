4aa0ba74559b0bd5070166f038aa53d80c47c812
Thomas Ricatte <thomas@ricatte.fr>
2020-02-28 10:00:06 +0100

    Bumping version to 0.2.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ language="c++", extra_compile_args=['-std=c++11', '-O0', '-g'], include_dirs=[nu
 
 setup(
     name = "fwg",
-    version="0.1.0",
+    version="0.2.0",
     author="Thomas Ricatte",
     description="Fast sliced wasserstein distance matrix computation",
     ext_modules=[extension_mod]
